f40e03b7ee22afd8669a8c6afc2acf3b6a9415a9
James Melling <jmzmelling@googlemail.com>
2022-12-22 22:33:14 +0000

    add openai endpoint + client fetch

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,25 @@ import { useState } from "react";
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -27,12 +46,33 @@ const Home = () => {
             onChange={onUserChangedText}
           />
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
               <div className="generate">
-                <p>Get Answers</p>
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Get Answers</p>
+                )}
               </div>
             </a>
           </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
