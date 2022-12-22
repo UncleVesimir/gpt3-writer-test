b63f16f78f51fc72650ea1eab4456cdfb5b8e2fb
James Melling <jmzmelling@googlemail.com>
2022-12-22 22:02:40 +0000

    update title. Change colors

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>The Universe Will Guide You</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Find answers to your problems. Realign with your true path.</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            placeholder="Start typing your questions here..."
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Get Answers</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
